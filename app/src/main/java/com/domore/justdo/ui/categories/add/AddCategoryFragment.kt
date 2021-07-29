7a3b06a00f80209625dff001abb66fe5046b3615
SomeSimpleThings <makesomethingsimple@gmail.com>
2021-07-29 18:52:06 +0300

    icons

--- app/src/main/java/com/domore/justdo/ui/categories/add/AddCategoryFragment.kt
+++ app/src/main/java/com/domore/justdo/ui/categories/add/AddCategoryFragment.kt
@@ -1,6 +1,8 @@
 package com.domore.justdo.ui.categories.add
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
